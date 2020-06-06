87fea83f9796280308e30c3757749f32a77553e1
nikhil <nikhil.bhavsar@amagi.com>
2020-06-06 22:32:37 +0530

    Added more router path

--- main.go
+++ main.go
@@ -95,5 +95,8 @@ func main() {
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/event", createEvent).Methods("POST")
 	router.HandleFunc("/events", getAllEvents).Methods("GET")
+	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
+	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8082", router))
 }
