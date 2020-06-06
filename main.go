b6dff15b71256666a17311202d8ab18a5202a311
nikhil <nikhil.bhavsar@amagi.com>
2020-06-06 22:23:26 +0530

    Added operation in main

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -13,7 +15,6 @@ type event struct {
 	Title       string `json:"Title"`
 	Description string `json:"Description"`
 }
-
 
 type allEvents []event
 
@@ -29,26 +30,24 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
 
-func createEvent(w http.ResponseWriter, r *http.Request){
+func createEvent(w http.ResponseWriter, r *http.Request) {
 	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
 
-	reqBody,err:= ioutil.ReadAll(r.Body)
-  if err!=nil{
-	  fmt.Fprintf(w,"Kindly enter data with the event title and discription only in order to update")
-  }
+	json.Unmarshal(reqBody, &newEvent)
 
-  
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
 
-
-
-
-
-
-
-
+	json.NewEncoder(w).Encode(newEvent)
+}
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/event", createEvent).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8082", router))
 }
